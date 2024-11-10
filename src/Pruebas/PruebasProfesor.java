23f60aefb08fc60dcff5dca38c73c617bcf1d535
victoriam2003 <v.merizaldec@uniandes.edu.co>
2024-11-10 12:24:53 -0500

    .

--- src/Pruebas/PruebasProfesor.java
+++ src/Pruebas/PruebasProfesor.java
@@ -19,6 +19,7 @@ import Actividades.Pregunta;
 import Actividades.Quiz;
 import Actividades.RecursoEducativo;
 import Actividades.Tarea;
+import Exceptions.NombreRepetido;
 import Perisistencia.PersistenciaActividades;
 import Perisistencia.PersistenciaLearningPaths;
 import Perisistencia.PersistenciaUsuarios;
@@ -36,12 +37,28 @@ class PruebasProfesor {
 	
 	/// Test de crear LearningPath
 	@Test
-	void testCrearLearningPath() {
+	void testCrearLearningPath() throws NombreRepetido {
 		List<String> intereses = List.of("programación","dpoo");
 		List<String> actividadesID = List.of("A_1","A_2","A_3");
 		profesorPrueba.crearLearningPath("LP_1¨", "Como hacer una persistencia", "descripción", "objetivos",3
 				, 120 , "P_1", actividadesID, intereses);
 		assertEquals(1, profesorPrueba.learningPathsCreados.size());
+	}
+	
+	/// Test de crear learningPath con nombre repetido
+	@Test
+	void testCrearLearningPathNombreRepetido() throws NombreRepetido {
+		List<String> intereses = List.of("programación", "dpoo");
+		List<String> actividadesID = List.of("A_1", "A_2", "A_3");
+		profesorPrueba.crearLearningPath("LP_1¨", "Como hacer una persistencia", "descripción", "objetivos",3
+				, 120 , "P_1", actividadesID, intereses);
+		boolean thrown = false;
+		try {
+			profesorPrueba.crearLearningPath("LP_1¨", "Como hacer una persistencia", "descripción", "objetivos", 3, 120,
+					"P_1", actividadesID, intereses);
+		} catch (NombreRepetido e) {
+			thrown = true;
+		}
 	}
 	
 	
@@ -238,10 +255,140 @@ class PruebasProfesor {
 	}
 	
 	/// Test de Revisar estado de Quiz
-	///Test de Revisar estado de Examen
-	///Test de Revisar estado de Tarea
-	///Test de Revisar estado de Encuesta
-	///Test de Revisar estado de Recurso Educativo
+	@Test
+	void testRevisarEstadoQuiz() throws ParseException {
+		ArrayList<String> actividadesPrevias = new ArrayList<String>();
+	actividadesPrevias.add("A101");
+	actividadesPrevias.add("A103");
 	
+	ArrayList<String> actividadesSeguimiento = new ArrayList<String>();
+	actividadesSeguimiento.add("A102");
+	
+	ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
+	ArrayList<String> respuestas = new ArrayList<String>();
+	respuestas.add("A");
+	respuestas.add("B");
+	respuestas.add("C");
+	
+	HashMap<String, Object> atributosEspecificos = new HashMap<String, Object >();
+	atributosEspecificos.put("calificacionMinima", 0.5);
+	atributosEspecificos.put("RespuestaCorrecta", respuestas.get(0));
+	
+	preguntas.add(new Pregunta("Para que es public, private, protected", respuestas));
+	atributosEspecificos.put("preguntas", preguntas);
+	
+	
+	String fecha = "2021-12-01";
+	Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+	
+	Actividad actividadCreada = (Quiz) profesorPrueba.crearActividad("A110", "Descripcion", "Objetivos", 3, 120, true, date, "reseña", 0, 0, "Quiz", "LP106", actividadesPrevias, actividadesSeguimiento, atributosEspecificos,"A103" );
+	profesorPrueba.revisarEstadoActividad("A_110", "LP106");
+	assertEquals("Fallido", actividadCreada.getEstado());
+}
+	
+	///Test de Revisar estado de Examen
+	@Test
+	void testRevisarEstadoExamen() throws ParseException {
+		ArrayList<String> actividadesPrevias = new ArrayList<String>();
+		actividadesPrevias.add("A101");
+		actividadesPrevias.add("A103");
+
+		ArrayList<String> actividadesSeguimiento = new ArrayList<String>();
+		actividadesSeguimiento.add("A102");
+
+		ArrayList<Pregunta> preguntas = new ArrayList<Pregunta>();
+
+		HashMap<String, Object> atributosEspecificos = new HashMap<String, Object>();
+		atributosEspecificos.put("calificacionMinima", 0.5);
+
+		preguntas.add(new Pregunta("Para que es public, private, protected", null));
+		atributosEspecificos.put("preguntas", preguntas);
+
+		String fecha = "2021-12-01";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+		Actividad actividadCreada = (Examen) profesorPrueba.crearActividad("A110", "Descripcion", "Objetivos", 3, 120,
+				true, date, "reseña", 0, 0, "Examen", "LP106", actividadesPrevias, actividadesSeguimiento,
+				atributosEspecificos, "A103");
+		profesorPrueba.revisarEstadoActividad("A_110", "LP106");
+		assertEquals("Fallido", actividadCreada.getEstado());
+	}
+	
+	///Test de Revisar estado de Tarea
+	@Test
+	void testRevisarEstadoTarea() throws ParseException {
+		ArrayList<String> actividadesPrevias = new ArrayList<String>();
+		actividadesPrevias.add("A101");
+		actividadesPrevias.add("A103");
+
+		ArrayList<String> actividadesSeguimiento = new ArrayList<String>();
+		actividadesSeguimiento.add("A102");
+
+		HashMap<String, Object> atributosEspecificos = new HashMap<String, Object>();
+		atributosEspecificos.put("tipoEntrega", "Archivo");
+		atributosEspecificos.put("tipoArchivo", "PDF");
+
+		String fecha = "2021-12-01";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+		Actividad actividadCreada = (Tarea) profesorPrueba.crearActividad("A110", "Descripcion", "Objetivos", 3, 120,
+				true, date, "reseña", 0, 0, "Tarea", "LP106", actividadesPrevias, actividadesSeguimiento,
+				atributosEspecificos, "A103");
+		profesorPrueba.revisarEstadoActividad("A_110", "LP106");
+		assertEquals("Fallido", actividadCreada.getEstado());
+	}
+	
+	///Test de Revisar estado de Encuesta
+	@Test
+	void testRevisarEstadoEncuesta() throws ParseException {
+		ArrayList<String> actividadesPrevias = new ArrayList<String>();
+		actividadesPrevias.add("A101");
+		actividadesPrevias.add("A103");
+
+		ArrayList<String> actividadesSeguimiento = new ArrayList<String>();
+		actividadesSeguimiento.add("A102");
+
+		ArrayList<String> preguntas = new ArrayList<String>();
+		preguntas.add("¿Te gusto la actividad?");
+
+		HashMap<String, Object> atributosEspecificos = new HashMap<String, Object>();
+		atributosEspecificos.put("preguntas", preguntas);
+
+		String fecha = "2021-12-01";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+		Actividad actividadCreada = (Encuesta) profesorPrueba.crearActividad("A110", "Descripcion", "Objetivos", 3, 120,
+				true, date, "reseña", 0, 0, "Encuesta", "LP106", actividadesPrevias, actividadesSeguimiento,
+				atributosEspecificos, "A103");
+		profesorPrueba.revisarEstadoActividad("A_110", "LP106");
+		assertEquals("Exitoso", actividadCreada.getEstado());
+	}
+	
+	///Test de Revisar estado de Recurso Educativo
+	@Test
+	void testRevisarEstadoRecursoEducativo() throws ParseException {
+		ArrayList<String> actividadesPrevias = new ArrayList<String>();
+		actividadesPrevias.add("A101");
+		actividadesPrevias.add("A103");
+
+		ArrayList<String> actividadesSeguimiento = new ArrayList<String>();
+		actividadesSeguimiento.add("A102");
+
+		HashMap<String, Object> atributosEspecificos = new HashMap<String, Object>();
+		atributosEspecificos.put("tipoRecurso", "Video");
+		atributosEspecificos.put("linkRecurso", "www.youtube.com");
+
+		String fecha = "2021-12-01";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+
+		Actividad actividadCreada = (RecursoEducativo) profesorPrueba.crearActividad("A110", "Descripcion", "Objetivos",
+				3, 120, true, date, "reseña", 0, 0, "Recurso Educativo", "LP106", actividadesPrevias,
+				actividadesSeguimiento, atributosEspecificos, "A103");
+		profesorPrueba.revisarEstadoActividad("A_110", "LP106");
+		assertEquals("Exitoso", actividadCreada.getEstado());
+	}
+	
+
 	
 }
